eb0ab9604fbef88f42de40177aa6d226a0ab1502
Azka Norouzi <a.azkanorouzi@gmail.com>
2024-05-14 15:29:12 +0330

    Add casing shoes

--- src/components/ProductShowCase/Product.jsx
+++ src/components/ProductShowCase/Product.jsx
@@ -1,5 +1,5 @@
-import case1 from '../../assets/cases1.png'
-import { useState, useEffect } from 'preact/hooks'
+import case1 from "../../assets/cases1.png";
+import { useState, useEffect } from "preact/hooks";
 
 export default function Product({
   title,
@@ -8,20 +8,20 @@ export default function Product({
   customStyles,
   href,
 }) {
-  const [hovered, setHovered] = useState(false)
+  const [hovered, setHovered] = useState(false);
 
   return (
     <figure
       class={`relative cursor-pointer overflow-hidden scale-100 lg:scale-95 transition-transform product ${customStyles} w-80 sm:w-auto`}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      style={{ transform: hovered ? 'scale(1)' : '', zIndex: '1' }}
+      style={{ transform: hovered ? "scale(1)" : "", zIndex: "1" }}
     >
       {children}
       <figcaption class="flex flex-col ">
         <div
           class={`absolute text-center flex justify-center items-center bottom-0 top-0 right-0 left-0 transition-colors bg-opacity-50 bg-forth  ${
-            hovered ? 'bg-forth' : ' bg-forth lg:bg-[rgba(255,255,255,0)]'
+            hovered ? "bg-forth" : " bg-forth lg:bg-[rgba(255,255,255,0)]"
           }`}
         >
           <div class="py-4 h-full flex flex-col justify-center">
@@ -29,11 +29,11 @@ export default function Product({
               <h3
                 class={`text-[2.5rem] lg:translate-y-10 opacity-100 lg:opacity-70 px-[78px] `}
                 style={{
-                  transition: '.5s',
-                  opacity: hovered ? '1' : '.5',
-                  transform: hovered ? 'translateY(0)' : '',
-                  letterSpacing: hovered ? '10px' : '0',
-                  lineHeight: '50px',
+                  transition: ".5s",
+                  opacity: hovered ? "1" : ".5",
+                  transform: hovered ? "translateY(0)" : "",
+                  letterSpacing: hovered ? "10px" : "0",
+                  lineHeight: "50px",
                 }}
               >
                 {title}
@@ -41,8 +41,8 @@ export default function Product({
               <p
                 class={`text-xl px-10 font-oswald lg:translate-x-[100%]`}
                 style={{
-                  transition: '.5s',
-                  transform: hovered ? 'translatex(0)' : '',
+                  transition: ".5s",
+                  transform: hovered ? "translatex(0)" : "",
                 }}
               >
                 {description}
@@ -51,8 +51,8 @@ export default function Product({
             <a
               class={`text-primary bg-red lg:-translate-x-[200%] hover:text-secondary rotate hover:scale-110 `}
               style={{
-                transition: '.5s',
-                transform: hovered ? 'translatex(0)' : '',
+                transition: ".5s",
+                transform: hovered ? "translatex(0)" : "",
               }}
               href={href}
             >
@@ -62,20 +62,20 @@ export default function Product({
         </div>
       </figcaption>
     </figure>
-  )
+  );
 }
 export function ProductStatic({ src, alt, title, href }) {
   return (
     <div class="relative">
-      <img src={src} alt={alt} class="lg:max-w-[350px] " />
+      <img src={src} alt={alt} class="lg:max-w-[350px] h-full" />
       <a
         class="bottom-0 right-0 left-0 bg-sixth py-4  lg:py-8 bg-opacity-45 backdrop-blur-sm absolute lg:text-2xl flex justify-center lg:gap-10 gap-5 items-center hover:text-primary transition-colors"
-        role={'button'}
+        role={"button"}
         href={href}
       >
         <p> {title} </p>
         <span>&rarr;</span>
       </a>
     </div>
-  )
+  );
 }
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,85 +1,87 @@
-import { useState, useEffect } from 'preact/hooks'
-import NavToggleBtn from './NavToggleBtn'
+import { useState, useEffect } from "preact/hooks";
+import NavToggleBtn from "./NavToggleBtn";
 
 export default function StickyNavbar({ children }) {
-  const [prevScrollPos, setPrevScrollPos] = useState(0)
-  const [visible, setVisible] = useState(true)
-  const [closed, setClosed] = useState(true)
-  const [scrollPos, setScrollPos] = useState(window.pageYOffset)
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const [closed, setClosed] = useState(true);
+  const [scrollPos, setScrollPos] = useState(window.pageYOffset);
   const isWhite =
     window.location.pathname.length === 1 ||
-    window.location.pathname.includes('contacts') ||
-    window.location.pathname.includes('about') ||
-    window.location.pathname.includes('projects') ||
-    window.location.pathname.includes('coringrig') || 
-    window.location.pathname.includes('rods') ||
-    window.location.pathname.includes('drillingAccessories') ||
-    window.location.pathname.includes('coreBarrels') ||
-    window.location.pathname.includes('reamingShells')
+    window.location.pathname.includes("contacts") ||
+    window.location.pathname.includes("about") ||
+    window.location.pathname.includes("projects") ||
+    window.location.pathname.includes("coringrig") ||
+    window.location.pathname.includes("rods") ||
+    window.location.pathname.includes("drillingAccessories") ||
+    window.location.pathname.includes("coreBarrels") ||
+    window.location.pathname.includes("reamingShells") ||
+    window.location.pathname.includes("coreBits") ||
+    window.location.pathname.includes("casingShoes");
 
   useEffect(() => {
     const handleScroll = () => {
-      setScrollPos(window.pageYOffset)
-      setVisible(prevScrollPos > scrollPos || scrollPos < 400)
-      setClosed(true)
-      setPrevScrollPos(scrollPos)
-    }
+      setScrollPos(window.pageYOffset);
+      setVisible(prevScrollPos > scrollPos || scrollPos < 400);
+      setClosed(true);
+      setPrevScrollPos(scrollPos);
+    };
 
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [prevScrollPos])
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
 
   useEffect(() => {
-    const currentPath = window.location.pathname
-    const links = document.querySelectorAll('.nav-link')
+    const currentPath = window.location.pathname;
+    const links = document.querySelectorAll(".nav-link");
 
     // Setting the active class for the navbar
     links.forEach(function (link) {
-      console.log(link.href)
-      if (link.getAttribute('href') === (currentPath.slice(0, -1) || '/')) {
-        link.classList.add(isWhite ? 'text-secondary' : 'text-sixth')
+      console.log(link.href);
+      if (link.getAttribute("href") === (currentPath.slice(0, -1) || "/")) {
+        link.classList.add(isWhite ? "text-secondary" : "text-sixth");
       }
-    })
+    });
     // Changing the navbar logo based on the current path
-    const navLogo = document.querySelector('.nav-logo')
-    const navLogoGrey = document.querySelector('.nav-logo-grey')
+    const navLogo = document.querySelector(".nav-logo");
+    const navLogoGrey = document.querySelector(".nav-logo-grey");
 
     // Hiding the navbar logo based on the current path (there will be two logos, grey - red, the red one is only apparent on index)
     if (isWhite) {
-      navLogoGrey.classList.add('hidden')
-    } else navLogo.classList.add('hidden')
-  }, [window.location.pathname])
+      navLogoGrey.classList.add("hidden");
+    } else navLogo.classList.add("hidden");
+  }, [window.location.pathname]);
 
   const navbarStyle = {
-    transform: visible ? 'translateY(0)' : 'translateY(-100%)',
-    transition: 'transform 0.3s ease-in-out',
+    transform: visible ? "translateY(0)" : "translateY(-100%)",
+    transition: "transform 0.3s ease-in-out",
     background:
       window.pageYOffset > 200 && visible && !isWhite
-        ? 'rgba(255,255,255,0.3)'
+        ? "rgba(255,255,255,0.3)"
         : window.pageYOffset > 200 && visible
-        ? 'rgba(0,0,0,0.3)'
-        : 'unset',
-    position: 'fixed',
-    backdropFilter: scrollPos > 200 ? 'blur(10px)' : 'unset',
-    '-webkit-backdrop-filter': scrollPos > 200 ? 'blur(10px)' : 'unset',
-    '-moz-backdrop-filter': scrollPos > 200 ? 'blur(10px)' : 'unset',
-    '-o-backdrop-filter': scrollPos > 200 ? 'blur(10px)' : 'unset',
-    transition: 'background 1s, all .3s',
+          ? "rgba(0,0,0,0.3)"
+          : "unset",
+    position: "fixed",
+    backdropFilter: scrollPos > 200 ? "blur(10px)" : "unset",
+    "-webkit-backdrop-filter": scrollPos > 200 ? "blur(10px)" : "unset",
+    "-moz-backdrop-filter": scrollPos > 200 ? "blur(10px)" : "unset",
+    "-o-backdrop-filter": scrollPos > 200 ? "blur(10px)" : "unset",
+    transition: "background 1s, all .3s",
     top: 0,
-    width: '100%',
+    width: "100%",
     zIndex: 60,
-    boxShadow: scrollPos > 200 ? '2px 2px 2px rgba(0,0,0,0.3)' : 'unset',
-  }
+    boxShadow: scrollPos > 200 ? "2px 2px 2px rgba(0,0,0,0.3)" : "unset",
+  };
 
   return (
     <nav
       class={`bg-opacity-30    border-secondary  flex transition-all  p-3 lg:py-0 lg:pr-0 xl:pl-16 lg:pl-5 navbar ${
-        isWhite ? 'text-secondary' : 'text-sixth'
+        isWhite ? "text-secondary" : "text-sixth"
       }`}
-      style={{ boxShadow: 'inner', ...navbarStyle }}
+      style={{ boxShadow: "inner", ...navbarStyle }}
     >
       {children}
       <NavToggleBtn closed={visible ? closed : true} setClosed={setClosed} />
     </nav>
-  )
+  );
 }
